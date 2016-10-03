5c76170aa4517fc12a260a9da68c57065fa90632
Charles Rethman <rethman@me.com>
2016-10-03 12:44:19 +0200

    Initial commit: setting up SQL and sampling directories

---
+++ sql/livezones_pops.sql
@@ -0,0 +1,27 @@
+SELECT
+	'''' || pr_name || '''' AS pr_name,
+	pr_code,
+	'''' || dc_name || '''' AS dc_name,
+	dc_code,
+	'''' || mn_name || '''' AS mn_name,
+	mn_code,
+	'''' || mp_name || '''' AS mp_name,
+	mp_code,
+	'''' || sp_name || '''' AS sp_name,
+	sp_code, 
+	zaf.demog_sas.sa_code AS sa_code,
+	total AS Pop,
+	zaf.demog_sas.lz_code AS lz_code,
+	'''' || lz_name || '''' AS lz_name,
+	'''' || lz_abbrev || '''' AS lz_abbrev,
+	'''' || zaf.demog_sas.lz_code || ': ' || lz_name || ' (' || lz_abbrev || ')''' AS lz
+FROM
+	zaf.demog_sas,
+	zaf.tbl_pop_age_gender,
+	zaf.tbl_livezones_list
+WHERE
+	zaf.demog_sas.sa_code = zaf.tbl_pop_age_gender.sa_code AND
+	zaf.demog_sas.lz_code = zaf.tbl_livezones_list.lz_code
+ORDER BY
+	pr_name, dc_name, mn_name, mp_name, sp_name, zaf.demog_sas.sa_code
+;
