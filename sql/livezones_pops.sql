51529c8401dfb5fbddbdb4c02e5980d58c7c5333
Charles Rethman <rethman@me.com>
2016-10-04 10:17:51 +0200

    Worked on population queries

--- sql/livezones_pops.sql
+++ sql/livezones_pops.sql
@@ -1,27 +1,58 @@
-SELECT
-	'''' || pr_name || '''' AS pr_name,
-	pr_code,
-	'''' || dc_name || '''' AS dc_name,
-	dc_code,
-	'''' || mn_name || '''' AS mn_name,
-	mn_code,
-	'''' || mp_name || '''' AS mp_name,
-	mp_code,
-	'''' || sp_name || '''' AS sp_name,
-	sp_code, 
-	zaf.demog_sas.sa_code AS sa_code,
-	total_m + total_f AS pop,
-	zaf.demog_sas.lz_code AS lz_code,
-	'''' || lz_name || '''' AS lz_name,
-	'''' || lz_abbrev || '''' AS lz_abbrev,
-	'''' || zaf.demog_sas.lz_code || ': ' || lz_name || ' (' || lz_abbrev || ')''' AS lz
-FROM
-	zaf.demog_sas,
-	zaf.tbl_pop_agegender_05y,
-	zaf.tbl_livezones_list
-WHERE
-	zaf.demog_sas.sa_code = zaf.tbl_pop_agegender_05y.sa_code AND
-	zaf.demog_sas.lz_code = zaf.tbl_livezones_list.lz_code
-ORDER BY
-	pr_name, dc_name, mn_name, mp_name, sp_name, zaf.demog_sas.sa_code
+COPY (
+	SELECT
+		'''' || pr_name || '''' AS pr_name,
+		pr_code,
+		'''' || dc_name || '''' AS dc_name,
+		f.dc_code,
+		'''' || mn_name || '''' AS mn_name,
+		mn_code,
+		'''' || mp_name || '''' AS mp_name,
+		mp_code,
+		'''' || sp_name || '''' AS sp_name,
+		sp_code,
+		f.sa_code AS sa_code,
+		total_m + total_f AS pop_size,
+		(total_m + total_f) * i.pop_c / m.pop_y AS pop_curr,
+		round((total_m + total_f) * i.pop_c / m.pop_y, 0) AS pop_curr_approx,
+		f.lz_code AS lz_code,
+		'''' || lz_name || '''' AS lz_name,
+		'''' || lz_abbrev || '''' AS lz_abbrev,
+		'''' || f.lz_code || ': ' || lz_name || ' (' || lz_abbrev || ')''' AS lz
+	FROM
+		zaf.demog_sas AS f,
+		zaf.tbl_pop_agegender_05y AS g,
+		zaf.tbl_livezones_list AS h,
+		(
+			SELECT dc_code, sum(pop) AS pop_c
+			FROM zaf.tbl_pop_proj, zaf.admin3_dists
+			WHERE
+					year_mid = 2016
+				AND
+					zaf.admin3_dists.dc_mdb_code = zaf.tbl_pop_proj.dc_mdb_code
+			GROUP BY dc_code
+		) AS i,
+		(
+			SELECT dc_code, sum(total_m + total_f) AS pop_y
+			FROM zaf.tbl_pop_agegender_05y AS j, zaf.demog_sas AS k
+			WHERE j.sa_code = k.sa_code
+			GROUP BY dc_code
+		) AS m
+	WHERE
+			f.sa_code = g.sa_code
+		AND
+			f.lz_code = h.lz_code
+		AND
+			f.dc_code = i.dc_code
+		AND
+			f.dc_code = m.dc_code
+		AND
+			(f.lz_code = 59101 OR f.lz_code = 59104 OR f.lz_code = 59201 OR f.lz_code = 59205 OR f.lz_code = 59303)
+	ORDER BY
+		pr_name, dc_name, mn_name, mp_name, sp_name, f.sa_code
+)
+TO
+	'/Users/Charles/Documents/hea_baselines/south_africa/baselines_surveys/2016_lp_mp/sampling/livezones_pops.csv'
+WITH (
+	FORMAT CSV, DELIMITER ',', HEADER TRUE
+)
 ;
