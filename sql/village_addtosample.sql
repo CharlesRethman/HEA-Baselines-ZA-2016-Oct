8fe2dd4e7fede9a8a02ce2c574a29bc75e1f05ad
Charles Rethman <rethman@me.com>
2016-10-07 13:06:49 +0200

    Changes to view_sampled_villages and maps

--- sql/village_addtosample.sql
+++ sql/village_addtosample.sql
@@ -1,42 +1,24 @@
 INSERT INTO zaf.tbl_sampled_villages (
 	village_name,
 	village_code,
---	main_place_name,
---	main_place_code,
---	municipality_name,
---	municipality_code,
---	municipality_mdb_c,
---	district_name,
---	district_code,
---	district_mdb_c,
---	province_name,
---	province_code,
---	province_mdb_c,
-	lz_code, 
+	lz_code,
 	survey,
 	sampled_date
 	)
 	SELECT DISTINCT
 		sp_name,
 		sp_code,
---		mp_name,
---		mp_code,
---		mn_name,
---		mn_code,
---		mn_mdb_c,
---		dc_name,
---		dc_code,
---		dc_mdb_c,
---		pr_name,
---		pr_code,
---		pr_mdb_c,
 		lz_code,
-		'hea' AS survey,
+		'continuum' AS survey,
 		date '2016-10-04' AS sampled_date
 	FROM
-		zaf.demog_sas 
-	WHERE 
-			lz_code = 59104
-		AND 
-			(sp_code = 860008001 OR sp_code = 860009002 OR sp_code = 860012001 OR sp_code = 860021001 OR sp_code = 860022001 OR sp_code = 860026001 OR sp_code = 860031001 OR sp_code = 860035001 OR sp_code = 860038001 OR sp_code = 860039001 OR sp_code = 860041001 OR sp_code = 860050001 ) 
+		zaf.demog_sas
+	WHERE
+			lz_code IN (
+				VALUES (59101), (59104), (59201), (59205), (59303)
+			)
+		AND
+			sp_code IN (
+				VALUES (872033001), (872017001), (871006005), (871016001), (862006001), (863005001), (872001001), (871004001)
+			)
 ;
